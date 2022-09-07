080a1ef7499d0a56fc1f798f5a32ea833fbab032
Зверев Андрей Алексеевич <zverevaa@mfcto.ru>
2022-09-07 10:14:54 +0500

    Статические и не статические методы

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Calculator {
+    private static int x = 5;
+
+    public static int minus(int y) {
+        return x - y;
+    }
+
+    public double divide(int y) {
+        return y / x;
+    }
+
+    public int multiply(int y) {
+        return x * y;
+    }
+
+    public double sumAllOperation(int y) {
+        return minus(y) + divide(y) + multiply(y);
+    }
+
+    public static void main(String[] args) {
+        int y = 10;
+        Calculator calculator = new Calculator();
+        System.out.println("Минус = " + minus(y));
+        System.out.println("Деление = " + calculator.divide(y));
+        System.out.println("Умножение = " + calculator.multiply(y));
+        System.out.println("Сумма операций = " + calculator.sumAllOperation(y));
+    }
+}
