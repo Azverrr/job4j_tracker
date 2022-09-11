88b2566f9b7ce09db196e6e4e285f68bb7064052
andrei_zverev <andreyzverev1987@gmail.com>
2022-09-11 14:03:49 +0500

    Массивы и модели.

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -2,27 +2,27 @@ package ru.job4j.pojo;
 
 public class Library {
     public static void main(String[] args) {
-        Book lordOfTheRing = new Book("Lord of the ring", 1200);
-        Book warAndPeace = new Book("Война и мир", 1500);
-        Book cleanCode = new Book("Clean code", 500);
-        Book[] books = new Book[3];
-        books[0] = lordOfTheRing;
-        books[1] = warAndPeace;
-        books[2] = cleanCode;
+        Book[] books = new Book[4];
+        books[0] = new Book("Lord of the ring", 1200);
+        books[1] = new Book("Война и мир", 1500);
+        books[2] = new Book("Clean code", 500);
+        books[3] = new Book("Проклятые короли", 600);
         for (Book tmp : books) {
             System.out.println("Название книги: " + tmp.getName() + ", " + "Количество страниц: "
                     + tmp.getNumberOfPage());
         }
         System.out.println("Replace index");
-        books[2] = lordOfTheRing;
-        books[0] = cleanCode;
+        Book temp = new Book("temp", 0);
+        temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
         for (Book tmp : books) {
             System.out.println("Название книги: " + tmp.getName() + ", " + "Количество страниц: "
                     + tmp.getNumberOfPage());
         }
         System.out.println("Show Clean code");
         for (Book tmp : books) {
-            if (tmp.getName().equals("Clean code")) {
+            if ("Clean code".equals(tmp.getName())) {
                 System.out.println("Название книги: " + tmp.getName() + ", " + "Количество страниц: "
                         + tmp.getNumberOfPage());
             }
