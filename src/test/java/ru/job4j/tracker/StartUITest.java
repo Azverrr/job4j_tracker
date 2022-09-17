74996f7df4b81b04d409af7066e6ca917b61739d
andrei_zverev <andreyzverev1987@gmail.com>
2022-09-17 21:05:46 +0500

    Реализация меню за счет шаблона стратегия.

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,40 +1,5 @@
 package ru.job4j.tracker;
 
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-
 class StartUITest {
 
-    @Test
-    void createItem() {
-        String[] answers = {"problem"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("problem");
-        assertThat(created.getName()).isEqualTo(expected.getName());
-    }
-
-    @Test
-    void editItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("Edit item");
-        tracker.add(item);
-        String[] answer = {String.valueOf(item.getId()), "Edit item"};
-        StartUI.editItem(new StubInput(answer), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName()).isEqualTo("Edit item");
-    }
-
-    @Test
-    void deleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("deleted item");
-        tracker.add(item);
-        String[] answer = {String.valueOf(item.getId()), "deleted item"};
-        StartUI.deleteItem(new StubInput(answer), tracker);
-        Item deleted = tracker.findById(item.getId());
-        assertThat(deleted).isEqualTo(null);
-    }
 }
