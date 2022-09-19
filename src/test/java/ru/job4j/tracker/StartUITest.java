81550fb9c4a889b66eb74e9bc28e2359585b84f4
andrei_zverev <andreyzverev1987@gmail.com>
2022-09-19 21:31:39 +0500

    Тесты вывода на консоль

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -50,4 +50,101 @@ import static org.assertj.core.api.Assertions.assertThat;
             new StartUI(output).init(in, tracker, actions);
             assertThat(tracker.findById(item.getId())).isNull();
         }
+
+        @Test
+        public void whenEditItemTestOutputIsSuccessfully() {
+            Output out = new StubOutput();
+            Tracker tracker = new Tracker();
+            Item one = tracker.add(new Item("test1"));
+            String replaceName = "New Test Name";
+            Input in = new StubInput(
+                    new String[] {"0", String.valueOf(one.getId()), replaceName, "1"}
+            );
+            UserAction[] actions = new UserAction[]{
+                    new EditAction(out),
+                    new ExitAction()
+            };
+            new StartUI(out).init(in, tracker, actions);
+            String ln = System.lineSeparator();
+            assertThat(out.toString()).isEqualTo(
+                             "0. Edit item" + ln
+                            + "1. Exit" + ln
+                            + "=== Edit item ===" + ln
+                            + "Заявка изменена успешно" + ln
+                            + "0. Edit item" + ln
+                            + "1. Exit" + ln
+            );
+        }
+
+        @Test
+        public void whenShowAllItemActionTestOutputIsSuccessfully() {
+            Output out = new StubOutput();
+            Tracker tracker = new Tracker();
+            Item one = tracker.add(new Item("test1"));
+            Input in = new StubInput(
+                    new String[] {"0", "1"}
+            );
+            UserAction[] actions = new UserAction[]{
+                    new ShowAllItem(out),
+                    new ExitAction()
+            };
+            new StartUI(out).init(in, tracker, actions);
+            String ln = System.lineSeparator();
+            assertThat(out.toString()).isEqualTo(
+                    "0. Show all items" + ln
+                            + "1. Exit" + ln
+                            + "=== Show all items ===" + ln
+                            + one + ln
+                            + "0. Show all items" + ln
+                            + "1. Exit" + ln
+            );
+        }
+
+        @Test
+        public void whenFindByNameActionTestOutputIsSuccessfully() {
+            Output out = new StubOutput();
+            Tracker tracker = new Tracker();
+            Item one = tracker.add(new Item("test1"));
+            Input in = new StubInput(
+                    new String[] {"0","test1", "1"}
+            );
+            UserAction[] actions = new UserAction[]{
+                    new FindByNameAction(out),
+                    new ExitAction()
+            };
+            new StartUI(out).init(in, tracker, actions);
+            String ln = System.lineSeparator();
+            assertThat(out.toString()).isEqualTo(
+                    "0. Find items by name" + ln
+                            + "1. Exit" + ln
+                            + "=== Find items by name ===" + ln
+                            + one + ln
+                            + "0. Find items by name" + ln
+                            + "1. Exit" + ln
+            );
+        }
+
+        @Test
+        public void whenFindByIdActionTestOutputIsSuccessfully() {
+            Output out = new StubOutput();
+            Tracker tracker = new Tracker();
+            Item one = tracker.add(new Item("test1"));
+            Input in = new StubInput(
+                    new String[] {"0",String.valueOf(one.getId()), "1"}
+            );
+            UserAction[] actions = new UserAction[]{
+                    new FindByIdAction(out),
+                    new ExitAction()
+            };
+            new StartUI(out).init(in, tracker, actions);
+            String ln = System.lineSeparator();
+            assertThat(out.toString()).isEqualTo(
+                    "0. Find item by id" + ln
+                            + "1. Exit" + ln
+                            + "=== Find item by id ===" + ln
+                            + one + ln
+                            + "0. Find item by id" + ln
+                            + "1. Exit" + ln
+            );
+        }
     }
