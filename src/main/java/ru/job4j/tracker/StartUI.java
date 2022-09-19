72f412f19df01dd0f8331cccf65b152b1950b81d
andrei_zverev <andreyzverev1987@gmail.com>
2022-09-19 20:57:34 +0500

    Зависимость от System.out

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,6 +1,11 @@
 package ru.job4j.tracker;
 
 public class StartUI {
+    private final Output out;
+
+    public StartUI(Output out) {
+        this.out = out;
+    }
 
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
@@ -15,22 +20,23 @@ public class StartUI {
     private void showMenu(UserAction[] actions) {
         System.out.println("Menu");
         for (int i = 0; i < actions.length; i++) {
-            System.out.println(i + ". " + actions[i].name());
+            out.println(i + ". " + actions[i].name());
         }
     }
 
     public static void main(String[] args) {
+        Output output = new ConsoleOutput();
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(),
-                new ShowAllItem(),
-                new EditAction(),
-                new DeleteAction(),
-                new FindByIdAction(),
-                new FindeByNameAction(),
+                new CreateAction(output),
+                new ShowAllItem(output),
+                new EditAction(output),
+                new DeleteAction(output),
+                new FindByIdAction(output),
+                new FindByNameAction(output),
                 new ExitAction()
         };
-        new StartUI().init(input, tracker, actions);
+        new StartUI(output).init(input, tracker, actions);
     }
 }
