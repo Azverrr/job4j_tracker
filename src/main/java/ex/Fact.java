0728bfcc2bec236992239fd65c80005dcaaf931a
Зверев Андрей Алексеевич <zverevaa@mfcto.ru>
2022-09-23 09:12:41 +0500

    Кидаем исключение - throw new RuntimeException

--- src/main/java/ex/Fact.java
+++ src/main/java/ex/Fact.java
@@ -2,10 +2,10 @@ package ex;
 
 public class Fact {
     public static int calc(int n) {
-        int rsl = 1;
         if (n < 0) {
             throw new IllegalArgumentException("N could not be less then 0");
         }
+        int rsl = 1;
         for (int index = 2; index <= n; index++) {
             rsl *= index;
         }
