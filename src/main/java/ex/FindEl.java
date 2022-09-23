aecb0d3b84cfec56dddbb230fd00da5ee23a007d
Зверев Андрей Алексеевич <zverevaa@mfcto.ru>
2022-09-23 14:14:23 +0500

    Пользовательские исключения.

--- src/main/java/ex/FindEl.java
+++ src/main/java/ex/FindEl.java
@@ -10,7 +10,7 @@ public class FindEl {
             }
         }
         if (rsl == -1) {
-            throw new ElementNotFoundException();
+            throw new ElementNotFoundException("Key not found");
         }
         return rsl;
     }
